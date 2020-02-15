218dd4b4c68d32771cc5a427d607e9d65f55d7b1
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-02-14 23:15:21 -0500

    Landing page updated, landing container (prelim) created.  Further updates pending.

--- src/components/imdb-component/imdb-search-displaly-component/ImdbSearchDisplayComponent.tsx
+++ src/components/imdb-component/imdb-search-displaly-component/ImdbSearchDisplayComponent.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { CardBody, Card, CardImg, CardTitle, CardText } from 'reactstrap';
 
 interface IImdbDisplayProps {
     Title: string
@@ -20,7 +19,7 @@ export class ImdbSearchDisplayComponent extends React.PureComponent<IImdbDisplay
                 <p><b>Released:</b> {this.props.Year}<br />
                 
                 <br />
-                <button className="mr-button">Add to Library</button> <button className="mr-button">Add to Watch List</button><br />
+                {/* <button className="mr-button">Add to Library</button> <button className="mr-button">Add to Watch List</button><br /> */}
                 <a href={`http://imdb.com/title/${this.props.imdbID}`} target="_blank" className="mr-button">View on IMDb</a></p>
                 
             </div>
